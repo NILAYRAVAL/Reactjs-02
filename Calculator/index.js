09cb5e521857d25944e049ada2f859fa718ab383
NILAYRAVAL <nilayraval16116@gmail.com>
2023-12-01 18:10:35 +0530

    Create Calculator using ReactJS & Bootstrap

---
+++ Calculator/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
+
+reportWebVitals();
